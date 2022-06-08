15e577081e187ca16b8c9fa33f83045384e9a71a
walterpaulo <walter0paulo@hotmail.com>
2022-06-08 13:05:59 -0300

    feat: added style global

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-import './App.css'
+
+import GlobalStyle from './styles/global'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <GlobalStyle />
     wainfo
     </>
   )
---
+++ src/styles/global.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle} from 'styled-components'
+
+export default createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap');
+
+*{
+    margin:0;
+    padding: 0;
+}
+
+body{
+    font-family: 'Inter', sans-serif;
+    font-weight: 300;
+    font-size: 16px;
+}
+`
