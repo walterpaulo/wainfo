023c89c130023c85a05bb0aeaee561424be6ed43
walterpaulo <walter0paulo@hotmail.com>
2022-06-08 23:13:07 -0300

    styled:

--- src/components/Layout/style.ts
+++ src/components/Layout/style.ts
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
 `
 export const Main = styled.div`
     height: var(100vh - 156px);
