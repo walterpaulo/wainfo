ca778213b787573ba22a42f2df1ec37cba3cf1cb
walterpaulo <walter0paulo@hotmail.com>
2022-07-07 00:06:42 -0300

    feat: api

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,13 +1,14 @@
-import { FormHandles } from '@unform/core';
-import { Form } from '@unform/web';
-import React, { useRef } from 'react';
-import { Button } from '../../ shared /components/Button/inde';
-import { InputBox } from '../../ shared /components/InputBox';
-import { Text4 } from '../../ shared /components/Text4';
-import { Container } from './style';
-import * as Yup from 'yup';
-import getValidationErrors from '../../util/getValidationErrors';
-import { Link } from 'react-router-dom';
+import { FormHandles } from "@unform/core";
+import { Form } from "@unform/web";
+import React, { useRef } from "react";
+import { Button } from "../../ shared /components/Button/inde";
+import { InputBox } from "../../ shared /components/InputBox";
+import { Text4 } from "../../ shared /components/Text4";
+import { Container } from "./style";
+import * as Yup from "yup";
+import getValidationErrors from "../../util/getValidationErrors";
+import { Link } from "react-router-dom";
+import { api } from "../../services/api/api";
 
 interface SignUnFormData {
   email: string;
@@ -17,19 +18,36 @@ interface SignUnFormData {
 function Signup() {
   const formRef = useRef<FormHandles>(null);
 
+  async function postUser(data: any) {
+    const response = await fetch(api+"/users", {
+      method: "POST",
+      body: data,
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+  }
+
   async function handleSubmit(data: SignUnFormData) {
     try {
       const schema = Yup.object().shape({
         name: Yup.string().required("Nome é obrigatório"),
         email: Yup.string().email().required("Email obrigatório"),
-        password: Yup.string().min(6).required("Senha inserir mínimo caractéres"),
+        password: Yup.string()
+          .min(6)
+          .required("Senha inserir mínimo caractéres"),
       });
       await schema.validate(data, {
         abortEarly: false,
       });
       // Validation passed
-      console.log(data);
-      console.log(formRef.current)
+      const obj = data
+      console.log(data)
+      const res = postUser(obj)
+      console.log(res);
+
+
+      formRef.current?.reset();
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
@@ -45,9 +63,9 @@ function Signup() {
     <Container>
       <Text4>Criar nova conta</Text4>
       <Form ref={formRef} onSubmit={handleSubmit}>
-        <InputBox name='name' type='text' placeholder='Nome'/>
-        <InputBox name='email' type='text' placeholder='Email'/>
-        <InputBox name='password' type='password' placeholder='Senha'/>
+        <InputBox name="name" type="text" placeholder="Nome" />
+        <InputBox name="email" type="text" placeholder="Email" />
+        <InputBox name="password" type="password" placeholder="Senha" />
         <Button>Criar</Button>
       </Form>
       <div>
